7fbae3939fe796f448da0ebdec063c4a30f60f32
github username <lhon001@gmail.com>
2018-10-09 21:26:52 -0400

    isPangram

--- isPangram.js
+++ isPangram.js
@@ -1,3 +1,4 @@
+// CodeWars 6 kyu
 // A pangram is a sentence that contains every single letter of the alphabet
 // at least once. For example, the sentence
 // "The quick brown fox jumps over the lazy dog" is a pangram,
@@ -7,19 +8,17 @@
 // Return True if it is, False if not. Ignore numbers and punctuation.
 
 function isPangram(str) {
-  let single = str.split(' ').join('')
+  let single = str.split(' ').join('').toLowerCase().replace(/[^a-z]/g, '')
   console.log(single)
   let obj = {}
 
-  for(let i of str){
+  for(let i of single){
     if(obj[i] === undefined){
       obj[i] = 1
-    } else {
-      obj[i] += 1
     }
   }
 
-  return (obj.length === 26) ? true : false
+  return (Object.keys(obj).length === 26) ? true : false
 }
 
 isPangram("helloWorld")
