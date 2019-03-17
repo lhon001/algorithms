3aea94af50413d55cd6d6b84be8e88ded9ba6cc2
github username <lhon001@gmail.com>
2019-03-17 16:31:27 -0400

    recursiveSumNestedArray

---
+++ recursiveSumNestedArray.js
@@ -0,0 +1,24 @@
+// Recursively add all elements in a nested array
+
+Array.prototype.first = function() {
+  return this[0];
+}
+
+Array.prototype.restOfArray = function() {
+  return this.splice(1);
+}
+
+function recSum(array) {
+  if ((array.length === 1) && (Array.isArray(array[0]))) {
+    return recSum(array[0]);
+  } else if((array.length === 1) && (!Array.isArray(array[0]))) {
+    return array[0];
+  } else if (!Array.isArray(array)) {
+    return array;
+  } else {
+    return recSum(array.first()) + recSum(array.restOfArray());
+  }
+}
+
+let arr = [1, [1, 1, [1, 1, [1, 1]]];
+recSum(arr);
