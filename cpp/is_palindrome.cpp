520f5061f40b9f1346a2eba269a334f22086a687
github username <lhon001@gmail.com>
2019-08-31 16:10:47 -0400

    is_palindrome in C++

---
+++ cpp/is_palindrome.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+std::string is_palindrome(std::string text) {
+  for (int i=1; i<=text.length(); i++) {
+    if (text[i - 1] != text[text.length() - i]) {
+      return "false";
+    }
+  }
+  return "true";
+}
+
+int main() {
+  std::cout << is_palindrome("madm");
+}
